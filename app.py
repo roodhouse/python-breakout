5f061abf8f5598881ef46e94dd18f3eb5fed0cba
roodhouse <rughjm@gmail.com>
2023-12-22 14:58:56 -0600

    init setup

---
+++ app.py
@@ -0,0 +1,21 @@
+# welcome screen
+    # graphic header
+    # play button
+    # high score button
+
+# game screen
+    # current lives
+    # current score
+    # pause button
+    # sound on/off button
+    # game board
+        # blocks
+        # ball
+        # bar
+
+# game over screen
+    # graphic header
+    # score
+    # enter iniitals to save high score
+    # high scores 
+    # play again
