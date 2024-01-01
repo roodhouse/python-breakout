8e1d0bc3622a237370c5159530c48d0eb9426adb
roodhouse <rughjm@gmail.com>
2024-01-01 13:10:44 -0600

    ball animation is done

--- app.py
+++ app.py
@@ -151,13 +151,119 @@ for i in range(13):
     neg_R = neg_R - 10
     row_neg.append(block)
 
+# test in next branch
+# for i in range(13):
+#     x_cor = -585 + i * 90
+#     block = Block()
+#     # block.penup()
+#     block.goto(x_cor, 300)
+#     block.change_color(back_R, 20, 147)
+#     block.sety(300)
+#     block.setx(x_cor)
+#     back_R = back_R - 10
+#     back_row.append(block)
+
+# for i in range(13):
+#     x_cor = -585 + i * 90
+#     block = Block()
+#     # block.penup()
+#     block.goto(x_cor, 250)
+#     block.change_color(four_R, 40, 147)
+#     block.sety(250)
+#     block.setx(x_cor)
+#     four_R = four_R - 10
+#     row_four.append(block)
+
+# for i in range(13):
+#     x_cor = -585 + i * 90
+#     block = Block()
+#     # block.penup()
+#     block.goto(x_cor, 200)
+#     block.change_color(three_R, 60, 147)
+#     block.sety(200)
+#     block.setx(x_cor)
+#     three_R = three_R - 10
+#     row_three.append(block)
+
+# for i in range(13):
+#     x_cor = -585 + i * 90
+#     block = Block()
+#     # block.penup()
+#     block.goto(x_cor, 150)
+#     block.change_color(two_R, 80, 147)
+#     block.sety(150)
+#     block.setx(x_cor)
+#     two_R = two_R - 10
+#     row_two.append(block)
+
+# for i in range(13):
+#     x_cor = -585 + i * 90
+#     block = Block()
+#     # block.penup()
+#     block.goto(x_cor, 100)
+#     block.change_color(one_R, 100, 147)
+#     block.sety(100)
+#     block.setx(x_cor)
+#     one_R = one_R - 10
+#     row_one.append(block)
+
+# for i in range(13):
+#     x_cor = -585 + i * 90
+#     block = Block()
+#     # block.penup()
+#     block.goto(x_cor, 50)
+#     block.change_color(zero_R, 120, 147)
+#     block.sety(50)
+#     block.setx(x_cor)
+#     zero_R = zero_R - 10
+#     row_zero.append(block)
+
+# for i in range(13):
+#     x_cor = -585 + i * 90
+#     block = Block()
+#     # block.penup()
+#     block.goto(x_cor, 0)
+#     block.change_color(neg_R, 140, 147)
+#     block.sety(0)
+#     block.setx(x_cor)
+#     neg_R = neg_R - 10
+#     row_neg.append(block)
+
+# for item in back_row:
+#     item.hideturtle()
+# for item in row_four:
+#     item.hideturtle()
+# for item in row_three:
+#     item.hideturtle()
+# for item in row_two:
+#     item.hideturtle()
+# for item in row_one:
+#     item.hideturtle()
+# for item in row_zero:
+#     item.hideturtle()
+# for item in row_neg:
+#     item.hideturtle()
+
 paddle = Paddle()
+paddle.hideturtle()
 ball = Ball()
+ball.hideturtle()
 
 screen.onkey(paddle.move_right, "Right")
 screen.onkey(paddle.move_left, "Left")
 
-game_on = True
+game_on = False
+
+def start():
+    global game_on, paddle, ball
+    print(game_on)
+    game_on = True
+    print(game_on)
+    paddle.showturtle()
+    ball.showturtle()
+    play()
+
+screen.onkey(start, 'space')
 
 all_rows = []
 all_rows.append(row_neg)
@@ -181,116 +287,117 @@ def winner():
     left_menu.clear()
     left_menu.write(f"x{LIVES}", font=("VT323", 35, "normal"))
 
-
 # condition for when the all_rows is completely empty
 
-while game_on:
-    if LIVES < 0:
-        left_menu.clear()
-        left_menu.write("GAME OVER", font=("VT323", 35, "normal"))
-        game_on = False
+def play():
+    global game_on, LIVES, SCORE, EMPTY
+    while game_on:
+        if LIVES < 0:
+            left_menu.clear()
+            left_menu.write("GAME OVER", font=("VT323", 35, "normal"))
+            game_on = False
 
-    else:
-        time.sleep(ball.move_speed)
-        screen.update()
-        ball.drop()
+        else:
+            time.sleep(ball.move_speed)
+            screen.update()
+            ball.drop()
 
-        # paddle hit
-        if ball.ycor() == -330 and ball.distance(paddle) < 150 or ball.ycor() > 375:
-            ball.bounce_y()
+            # paddle hit
+            if ball.ycor() == -330 and ball.distance(paddle) < 150 or ball.ycor() > 375:
+                ball.bounce_y()
 
-        # wall hit
-        if ball.xcor() == -590 or ball.xcor() == 580:
-            ball.wall_hit()
+            # wall hit
+            if ball.xcor() == -590 or ball.xcor() == 580:
+                ball.wall_hit()
 
-        # ball miss
-        if ball.ycor() == -450:
-            LIVES -= 1
-            if LIVES >= 0:
-                print('the whiff')
-                paddle.reset()
-                ball.reset()
-                left_menu.clear()
-                left_menu.write(f"x{LIVES}", font=("VT323", 35, "normal"))
-            else:
-                left_menu.clear()
-                left_menu.write("GAME OVER", font=("VT323", 35, "normal"))
-            
-        # block hit
-        for row in all_rows:
-            for index, block in enumerate(row):
-                if (
-                    ball.ycor() >= block.ycor() - 40
-                    and ball.ycor() <= block.ycor()
-                    and ball.xcor() >= block.xcor()
-                    and ball.xcor() <= block.xcor() + 80
-                ):
-                    ball.bounce_y()
-                    block.clear()
-                    row.pop(index)
-                    
-                    if row == all_rows[0]:
-                        ball.move_speed = ball.move_speed
-                        SCORE += 1
-                        right_menu.clear()
-                        right_menu.write(SCORE, font=("VT323", 35, "normal"))
-
-                    if len(row) == 0:
-                        EMPTY += 1
-                        print('row removed')
-                        if EMPTY == 7:
-                            winner()
+            # ball miss
+            if ball.ycor() == -450:
+                LIVES -= 1
+                if LIVES >= 0:
+                    print('the whiff')
+                    paddle.reset()
+                    ball.reset()
+                    left_menu.clear()
+                    left_menu.write(f"x{LIVES}", font=("VT323", 35, "normal"))
+                else:
+                    left_menu.clear()
+                    left_menu.write("GAME OVER", font=("VT323", 35, "normal"))
+                
+            # block hit
+            for row in all_rows:
+                for index, block in enumerate(row):
+                    if (
+                        ball.ycor() >= block.ycor() - 40
+                        and ball.ycor() <= block.ycor()
+                        and ball.xcor() >= block.xcor()
+                        and ball.xcor() <= block.xcor() + 80
+                    ):
+                        ball.bounce_y()
+                        block.clear()
+                        row.pop(index)
                         
-                    elif row == all_rows[1]:
-                        SCORE += 2
-                        right_menu.clear()
-                        right_menu.write(SCORE, font=("VT323", 35, "normal"))
-                        if ball.move_speed <= 0.06:
+                        if row == all_rows[0]:
                             ball.move_speed = ball.move_speed
+                            SCORE += 1
+                            right_menu.clear()
+                            right_menu.write(SCORE, font=("VT323", 35, "normal"))
+
+                        if len(row) == 0:
+                            EMPTY += 1
+                            print('row removed')
+                            if EMPTY == 7:
+                                winner()
+                            
+                        elif row == all_rows[1]:
+                            SCORE += 2
+                            right_menu.clear()
+                            right_menu.write(SCORE, font=("VT323", 35, "normal"))
+                            if ball.move_speed <= 0.06:
+                                ball.move_speed = ball.move_speed
+                            else:
+                                ball.move_speed = 0.06
+                        elif row == all_rows[2]:
+                            SCORE += 3
+                            right_menu.clear()
+                            right_menu.write(SCORE, font=("VT323", 35, "normal"))
+                            if ball.move_speed <= 0.05:
+                                ball.move_speed = ball.move_speed
+                            else:
+                                ball.move_speed = 0.05
+                        elif row == all_rows[3]:
+                            SCORE += 4
+                            right_menu.clear()
+                            right_menu.write(SCORE, font=("VT323", 35, "normal"))
+                            if ball.move_speed <= 0.04:
+                                ball.move_speed = ball.move_speed
+                            else:
+                                ball.move_speed = 0.04
+                        elif row == all_rows[4]:
+                            SCORE += 5
+                            right_menu.clear()
+                            right_menu.write(SCORE, font=("VT323", 35, "normal"))
+                            if ball.move_speed <= 0.03:
+                                ball.move_speed = ball.move_speed
+                            else:
+                                ball.move_speed = 0.03
+                        elif row == all_rows[5]:
+                            SCORE += 6
+                            right_menu.clear()
+                            right_menu.write(SCORE, font=("VT323", 35, "normal"))
+                            if ball.move_speed <= 0.02:
+                                ball.move_speed = ball.move_speed
+                            else:
+                                ball.move_speed = 0.02
+                        elif row == all_rows[6]:
+                            SCORE += 7
+                            right_menu.clear()
+                            right_menu.write(SCORE, font=("VT323", 35, "normal"))
+                            if ball.move_speed <= 0.01:
+                                ball.move_speed = ball.move_speed
+                            else:
+                                ball.move_speed = 0.01
                         else:
-                            ball.move_speed = 0.06
-                    elif row == all_rows[2]:
-                        SCORE += 3
-                        right_menu.clear()
-                        right_menu.write(SCORE, font=("VT323", 35, "normal"))
-                        if ball.move_speed <= 0.05:
-                            ball.move_speed = ball.move_speed
-                        else:
-                            ball.move_speed = 0.05
-                    elif row == all_rows[3]:
-                        SCORE += 4
-                        right_menu.clear()
-                        right_menu.write(SCORE, font=("VT323", 35, "normal"))
-                        if ball.move_speed <= 0.04:
-                            ball.move_speed = ball.move_speed
-                        else:
-                            ball.move_speed = 0.04
-                    elif row == all_rows[4]:
-                        SCORE += 5
-                        right_menu.clear()
-                        right_menu.write(SCORE, font=("VT323", 35, "normal"))
-                        if ball.move_speed <= 0.03:
-                            ball.move_speed = ball.move_speed
-                        else:
-                            ball.move_speed = 0.03
-                    elif row == all_rows[5]:
-                        SCORE += 6
-                        right_menu.clear()
-                        right_menu.write(SCORE, font=("VT323", 35, "normal"))
-                        if ball.move_speed <= 0.02:
-                            ball.move_speed = ball.move_speed
-                        else:
-                            ball.move_speed = 0.02
-                    elif row == all_rows[6]:
-                        SCORE += 7
-                        right_menu.clear()
-                        right_menu.write(SCORE, font=("VT323", 35, "normal"))
-                        if ball.move_speed <= 0.01:
-                            ball.move_speed = ball.move_speed
-                        else:
-                            ball.move_speed = 0.01
-                    else:
-                        print('no')
+                            print('no')
 
 
 screen.mainloop()
